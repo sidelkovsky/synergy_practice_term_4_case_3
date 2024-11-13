fe2085e2c997ffeebc959656dc0d94da7e217e7d
Sidelkovsky <sidelkovsky@tuta.io>
2024-11-14 03:06:16 +0400

    Caption "Image #".

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-let nextButton = document.getElementById('slide-arrow-next');
-let prevButton = document.getElementById('slide-arrow-prev');
+const nextButton = document.getElementById('slide-arrow-next');
+const prevButton = document.getElementById('slide-arrow-prev');
+const sliderItems = document.getElementById('slides-container');
+const sliderCaption = document.getElementById('sliderCaption');
+let imageCounter = 1;
 
 function nextSlide() {
-    console.log('Next Clicked' + Date.now())
+    console.log('Next Clicked' + Date.now());
+    imageCounter++;
+    if (imageCounter > sliderItems.childElementCount) {
+        imageCounter = 1;
+    }
+    sliderCaption.textContent = "Image " + imageCounter;
+    console.log(imageCounter);
 }
 
 function prevSlide() {
     console.log('Prev Clicked' + Date.now())
+    imageCounter--;
+    if (imageCounter < 1) {
+        imageCounter = sliderItems.childElementCount;
+    }
+    sliderCaption.textContent = "Image " + imageCounter;
+    console.log(imageCounter);
 }
 
 nextButton.addEventListener('click', nextSlide, false);

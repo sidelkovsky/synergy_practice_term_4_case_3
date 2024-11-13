0622d4927dc939ea45096cdabbb77fbf15b6a792
Sidelkovsky <sidelkovsky@tuta.io>
2024-11-14 03:48:31 +0400

    Change slides.

--- script.js
+++ script.js
@@ -2,26 +2,26 @@ const nextButton = document.getElementById('slide-arrow-next');
 const prevButton = document.getElementById('slide-arrow-prev');
 const sliderItems = document.getElementById('slides-container');
 const sliderCaption = document.getElementById('sliderCaption');
+const slidesContainer = document.getElementById('slides-container');
 let imageCounter = 1;
+let slideWidth = 640; // hardocded, todo: get wrapper width or image width?
 
 function nextSlide() {
-    console.log('Next Clicked' + Date.now());
     imageCounter++;
     if (imageCounter > sliderItems.childElementCount) {
         imageCounter = 1;
     }
+    slidesContainer.style.left = -slideWidth*(imageCounter-1) + "px";
     sliderCaption.textContent = "Image " + imageCounter;
-    console.log(imageCounter);
 }
 
 function prevSlide() {
-    console.log('Prev Clicked' + Date.now())
     imageCounter--;
     if (imageCounter < 1) {
         imageCounter = sliderItems.childElementCount;
     }
+    slidesContainer.style.left = -slideWidth*(imageCounter-1) + "px";
     sliderCaption.textContent = "Image " + imageCounter;
-    console.log(imageCounter);
 }
 
 nextButton.addEventListener('click', nextSlide, false);
